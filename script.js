678b4d354d5911b1ef060a868392885ace49a4c7
Peyton Brimmer <YOUR_EMAIL>
2024-04-10 19:07:34 -0400

    small commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log ("hello")
+
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+   
+    const APIKey = "caedcef682000464c021dc67bafa2853"
+     
+ 
